c35d1291568dc5e2ad0b3de1b3a3bc9e327d32b1
bengehya <princebengehya@gmail.com>
2025-03-18 15:43:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id "org.sonarqube" version "4.3.0.3225"
+    id("org.sonarqube") version "4.3.0.3225"
 }
 
 android {
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -44,23 +43,19 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     //Splash Screen API
-    implementation ("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
-    implementation ("com.google.android.material:material:1.4.0")
-
-
-
+    implementation("com.google.android.material:material:1.4.0")
 }
 
 sonarqube {
     properties {
-        property "sonar.projectKey", "myhospital"
-        property "sonar.projectName", "MyHospital"
-        property "sonar.host.url", "http://localhost:9000" 
-        property "sonar.login", "sqa_2ccc15a8d09c5b160342b49251706e8f48d2be65"
-        property "sonar.sources", "src/main/java" // Définit où se trouvent tes fichiers source
-        property "sonar.language", "java"
-        property "sonar.sourceEncoding", "UTF-8"
+        property("sonar.projectKey", "myhospital")
+        property("sonar.projectName", "MyHospital")
+        property("sonar.host.url", "http://localhost:9000") 
+        property("sonar.login", "sqa_2ccc15a8d09c5b160342b49251706e8f48d2be65")
+        property("sonar.sources", "src/main/java") // Définit où se trouvent tes fichiers source
+        property("sonar.language", "java")
+        property("sonar.sourceEncoding", "UTF-8")
     }
 }
-
