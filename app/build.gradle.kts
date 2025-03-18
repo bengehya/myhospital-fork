7ff6692555177e306641857822599314db026365
bengehya <princebengehya@gmail.com>
2025-03-18 19:36:59 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.0" // Updated AGP version
+    alias(libs.plugins.android.application)
     id("org.sonarqube") version "4.3.0.3225"
-    id("jacoco")
 }
 
 android {
@@ -14,6 +13,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -25,11 +25,7 @@ android {
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isTestCoverageEnabled = true // Correct property name
-        }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -37,27 +33,29 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-}
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.core.splashscreen)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
-jacoco {
-    toolVersion = "0.8.7" // Specify Jacoco version
+    //Splash Screen API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    implementation("com.google.android.material:material:1.4.0")
 }
 
 sonarqube {
     properties {
         property("sonar.projectKey", "myhospital")
         property("sonar.projectName", "MyHospital")
-        property("sonar.host.url", "http://localhost:9000")
-        property("sonar.login", System.getenv("SONAR_TOKEN"))
-        property("sonar.sources", "src/main/java, src/main/kotlin")
+        property("sonar.host.url", "http://localhost:9000") 
+        property("sonar.login", "sqa_2ccc15a8d09c5b160342b49251706e8f48d2be65")
+        property("sonar.sources", "src/main/java") // Définit où se trouvent tes fichiers source
         property("sonar.language", "java")
         property("sonar.sourceEncoding", "UTF-8")
-        property("sonar.junit.reportPaths", "build/test-results/testDebugUnitTest/TEST-*.xml")
-        property("sonar.jacoco.reportPaths", "build/jacoco/testDebugUnitTest.exec")
     }
 }
