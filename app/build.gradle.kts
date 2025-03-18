2fb9731bc04cab3eb6b62b2eef0ff4ec9190488f
bengehya <princebengehya@gmail.com>
2025-03-18 19:51:58 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     id("org.sonarqube") version "4.3.0.3225"
-    id("com.android.application") version "8.8.0"
+    id("com.android.application") version "8.1.0"
+
 }
 
 
