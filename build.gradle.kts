0732eb5527db4f4888257c38d43b14ae992b0e35
bengehya <princebengehya@gmail.com>
2025-04-03 00:43:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,4 @@ plugins {
     alias(libs.plugins.android.application) apply false
 }
 
-plugins {
-  id("org.sonarqube") version "6.0.1.5171"
-}
-    
-sonar {
-  properties {
-    property("sonar.projectKey", "myhospital1")
-    property("sonar.projectName", "myhospital1")
-  }
-}
+
