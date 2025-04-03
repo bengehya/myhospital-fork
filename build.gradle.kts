7e85eeedf5c10255477c9a2019c8eaf033eeed34
bengehya <princebengehya@gmail.com>
2025-04-03 23:40:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.sonarqube") version "4.3.0.3225" // Add SonarQube inside the same block
+    id("org.sonarqube") version "4.3.0.3225" // Apply SonarQube plugin
 }
 
 sonarqube {
     properties {
-        property("sonar.projectKey", "terraform-pipeline") 
-        property("sonar.host.url", "http://localhost:9000") 
-        property("sonar.token", System.getenv("SONAR_TOKEN")) 
+        property("sonar.projectKey", "terraform-pipeline") // Update with your actual project key
+        property("sonar.host.url", "http://localhost:9000") // Update if your SonarQube runs elsewhere
+        property("sonar.token", System.getProperty("sonar.token") ?: System.getenv("SONAR_TOKEN")) // Ensure token is set
     }
 }
+
 
 
 
