dcb58567f613a22fc1c55b276c763ad39b4bce23
bengehya <princebengehya@gmail.com>
2025-04-03 20:43:07 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("org.sonarqube") version "4.3.0.3225" // Add SonarQube inside the same block
 }
 
-// Configure SonarQube properties
-sonarqube {
-    properties {
-        property("sonar.projectKey", "myhospital1")
-        property("sonar.organization", "myhospital1") // Only needed for SonarCloud
-        property("sonar.host.url", "http://your-sonarqube-server.com") // Update if self-hosted
-        property("sonar.login", System.getenv("SONAR_TOKEN")) // Using environment variable
-    }
-}
+
