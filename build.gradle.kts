9256db9f971bad10b75dcb2127060b54bdc84a60
bengehya <princebengehya@gmail.com>
2025-04-03 20:44:50 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("org.sonarqube") version "4.3.0.3225" // Add SonarQube inside the same block
 }
+
+// Configure SonarQube properties
+sonarqube {
+    properties {
+        property("sonar.projectKey", "your_project_key")
+        property("sonar.organization", "your_organization") // Only needed for SonarCloud
+        property("sonar.host.url", "http://your-sonarqube-server.com") // Update if self-hosted
+        property("sonar.login", System.getenv("SONAR_TOKEN")) // Using environment variable
+    }
+}
+
 
 
