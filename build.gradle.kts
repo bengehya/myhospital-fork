02657f3b9c0663ce60c8c7f8413b50740ca07af1
bengehya <princebengehya@gmail.com>
2025-04-03 23:11:46 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,14 @@ plugins {
     id("org.sonarqube") version "4.3.0.3225" // Add SonarQube inside the same block
 }
 
-// Configure SonarQube properties
 sonarqube {
     properties {
-        property("sonar.projectKey", "your_project_key")
-        property("sonar.organization", "your_organization") // Only needed for SonarCloud
-        property("sonar.host.url", "http://your-sonarqube-server.com") // Update if self-hosted
-        property("sonar.login", System.getenv("SONAR_TOKEN")) // Using environment variable
+        property("sonar.projectKey", "terraform-pipeline") 
+        property("sonar.host.url", "http://localhost:9000") 
+        property("sonar.token", System.getenv("SONAR_TOKEN")) 
     }
 }
+
 
 
 
